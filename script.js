91e3fad4460fa191a6dcef67dfdc23d8ddde35cc
tuhin3054 <arifurrahman0865@gmail.com>
2022-09-29 23:46:15 +0600

    review section done

--- script.js
+++ script.js
@@ -35,4 +35,28 @@ var swiper = new Swiper(".home-slider", {
     }
 });
 
-  //    Swipe Initialize Swiper ends
+//    Swipe Initialize Swiper ends
+
+
+var swiper = new Swiper(".review-slider", {
+    slidesPerView: 1,
+    grabCursor: true,
+    loop: true,
+    spaceBetween: 10,
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        700: {
+            slidesPerView: 2,
+        },
+        1050: {
+            slidesPerView: 3,
+        },
+    },
+
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    }
+});
